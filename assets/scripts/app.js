8326cf0bf85507b8942f5e2df70db0204cbb2c03
aqbased <aqbased@hotmail.com>
2021-03-04 19:23:26 -0600

    Add auth signin, change pass, and sign up

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,12 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+const authEvents = require('./auth/events')
 
 $(() => {
-  // your JS code goes here
+  // Sign Up
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  // Sign In
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  // Change Password
+  $('#change-password').on('submit', authEvents.onChangePassword)
 })
---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,35 @@
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in',
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    data: data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword
+}
